36dcd5a49a4377b82c76c76748259f1fffe5f85d
Dusan Simic <dusansimic202gmail.com>
2021-03-14 13:50:01 +0100

    implement Dream CRUD endpoints

--- routes/api/dreams.js
+++ routes/api/dreams.js
@@ -14,6 +14,7 @@ router.get('/', async (req, res) => {
     }
 });
 
+
 // Create Dream
 router.post('/', async (req, res) => {
     const dream = new Dreams({
@@ -31,5 +32,67 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message})
     }
 });
+
+// Getting One Dream
+router.get('/:id', async (req, res) => {
+    let single_dream;
+
+    try{
+        single_dream = await Dreams.findById(req.params.id)
+        if(single_dream == null ){
+            return res.status(404).json( {message: 'Cannot find subscriber'})
+        }
+        res.send(single_dream)
+
+    }catch (err){
+        return res.status(500).json( { message: err.message })
+    }
+
+})
+
+// Update Dream
+router.put('/:id', async (req, res) => {
+
+    try{
+        let single_dream;
+
+        single_dream = await Dreams.findById(req.params.id)
+        console.log(single_dream)
+        if(single_dream === null ){
+            return res.status(404).json( {message: 'Cannot find dream'})
+        }
+        if(req.body.title !== null) {
+            single_dream.title = req.body.title
+        }
+        if(req.body.description !== null){
+            single_dream.description = req.body.description
+        }
+        if(req.body.type !== null){
+            single_dream.type = req.body.type
+        }
+        const updatedDream = await single_dream.save()
+
+        res.send(updatedDream)
+
+    }catch (err){
+        return res.status(500).json( { message: err.message })
+    }
+
+});
+
+// Delete  Dream
+router.delete('/:id', async (req, res) => {
+    try{
+        let single_dream;
+        single_dream = await Dreams.findById(req.params.id)
+
+        await single_dream.remove();
+        res.json( { message: 'Dream deleted' })
+    }catch (err){
+        res.status(500).json( { message: err.message })
+    }
+
+});
+
 
 module.exports = router;
